27ec7fc5d4c8a2ffd847fb79584aea189a16bab2
Shirlon Banks <sssbanks@gmail.com>
2017-07-15 12:30:13 -0700

    debug

--- inventory.sql
+++ inventory.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE Bamazon;
+CREATE DATABASE bamazon;
+
 
 USE bamazon;
 
@@ -6,75 +7,75 @@ CREATE TABLE Products (
 ItemID int NOT NULL,
 ProductName varchar(50) NOT NULL,
 DepartmentName varchar(50) NOT NULL,
-Price DECIMAL(4,2) NOT NULL,
+Price DECIMAL(5,2) NOT NULL,
 StockQuantity int NOT NULL);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-12345,
-'Duvet Cover',
-'Home',
-89.99,
-25);
+92116,
+'Darth Vader Pez Dispenser',
+'Toys',
+4.99,
+220);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-67891,
-'Mens Zip-up Hoodie',
+91730,
+'Mens Cargo Shorts',
 'Mens Clothing',
-29.99,
-25);
+19.99,
+250);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-23456,
-'Glass Catch-all Dish',
+92392,
+'Vitamix Blender 5200',
 'Home',
-15.99,
-10);
+499.99,
+100);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-78912,
-'Record Player',
+77282,
+'Hero Session GoPro',
 'Electronics',
-110.99,
-8);
+150.99,
+80);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-34567,
-'High Rise Skinny Jeans - Black',
+92485,
+'Floral Romper - XLarge',
 'Womens Clothing',
-59.99,
-30);
+39.99,
+38);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-89123,
+29294,
 'Womens Bruce Springsteen Baseball T-shirt',
 'Womens Clothing',
 25.99,
-8);
+83);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-45678,
+48459,
 'Mens Knit Beanie - Blue',
 'Mens Clothing',
 21.99,
-15);
+159);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 91234,
 'Striped Jersey Cotton Sheets - Queen',
 'Home',
 49.99,
-10);
+103);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 56789,
 'Spiked Dog Leash',
 'Pets',
 25.99,
-10);
+102);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 11234,
 'Shark Kitty Bed',
 'Pets',
 59.99,
-5);
+53);
